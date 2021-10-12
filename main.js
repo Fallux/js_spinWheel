8b0650b51df2353277d2fdfe5b478839bf2f6aed
Roemer Naber <200611@student.glu.nl>
2021-10-12 20:59:00 +0200

    één array teveel neergezet...

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("testetstetst");
 
-let getallen = [1,2,3,4,5,6,7,8,9];
+let getallen = [1,2,3,4,5,6,7,8];
 
 for (let i = 0; i < getallen.length; i++) {
     console.log(getallen[i]);
@@ -8,8 +8,7 @@ for (let i = 0; i < getallen.length; i++) {
     let elementDiv = document.createElement("DIV");
     elementDiv.innerHTML = getallen[i]; 
     elementDiv.style.backgroundColor="red";
-    // elementDiv.className="radBox";
-    elementDiv.classList.add("elementDiv");
+    elementDiv.className="radBox";
     elementDiv.style.transform= "rotate("+360/getallen.length*i+"deg)";
     if(i == 6){
         elementDiv.style.backgroundColor="green";
