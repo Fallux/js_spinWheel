8dc7d7fdab51672b6dfa4b577c38fefea54f9b3c
Roemer Naber <200611@student.glu.nl>
2021-10-12 20:42:51 +0200

    veel oefenen met de rotaties (zie images folder)

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ console.log("testetstetst");
 
 let getallen = [1,2,3,4,5,6,7,8,9];
 
-for (let i = 0; i < getallen.length; i= i+1) {
+for (let i = 0; i < getallen.length; i++) {
     console.log(getallen[i]);
 
     let elementDiv = document.createElement("DIV");
     elementDiv.innerHTML = getallen[i]; 
     elementDiv.style.backgroundColor="red";
-    elementDiv.className="radBox";
-    elementDiv.style.transform= "rotate(20deg)";
+    // elementDiv.className="radBox";
+    elementDiv.classList.add("elementDiv");
+    elementDiv.style.transform= "rotate("+360/getallen.length*i+"deg)";
     if(i == 6){
         elementDiv.style.backgroundColor="green";
         elementDiv.innerHTML += " getal geselecteerd ";
+        // elementDiv.style.transform= "rotate(20deg)"; aanrader als beginner maar dat kost te veel tijd en energie besteding
     }
     document.body.appendChild(elementDiv);  
 }
