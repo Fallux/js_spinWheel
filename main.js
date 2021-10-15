11059dbd59aba26f1b1176a05d5f3bb5cbcb7041
Roemer Naber <200611@student.glu.nl>
2021-10-15 12:33:27 +0200

    end result

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 console.log("testetstetst");
 let elementwheelContainer = document.getElementById("wheelContainer");
-let getallen = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+let getallen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 for (let i = 0; i < getallen.length; i++) {
     console.log(getallen[i]);
 
     let elementDiv = document.createElement("DIV");
-    elementDiv.innerHTML = getallen[i]; 
-    elementDiv.style.backgroundColor="red";
-    elementDiv.className="radBox";
-    elementDiv.style.transform= "rotate("+360/getallen.length*i+"deg)";
-    if(i == 6){
-        elementDiv.style.backgroundColor="green";
+    elementDiv.innerHTML = getallen[i];
+    elementDiv.style.backgroundColor = "red";
+    elementDiv.className = "radBox";
+    elementDiv.style.transform = "rotate(" + 360 / getallen.length * i + "deg)";
+    if (i == 6) {
+        elementDiv.style.backgroundColor = "green";
         elementDiv.innerHTML += " getal geselecteerd ";
         // elementDiv.style.transform= "rotate(20deg)"; aanrader als beginner maar dat kost te veel tijd en energie besteding
     }
-    elementwheelContainer.appendChild(elementDiv);  
+    elementwheelContainer.appendChild(elementDiv);
 }
 
 //knop maken
@@ -34,9 +34,15 @@ function mouseOverFunction() {
 // score
 let elementScore = document.getElementById("score");
 
- function onClickFunction(){
-  document.body.style.backgroundColor="purple";
-  elementwheelContainer.style.animationName = "rotate";
-  elementScore.innerHTML = Math.floor(Math.random() * (getallen.length)+1);
+function onClickFunction() {
+    document.body.style.backgroundColor = "purple";
+    if (elementwheelContainer.style.animationName == "") {
+        elementwheelContainer.style.animationName = "rotate";
+    }
+    else {
+        elementwheelContainer.style.animationName = "";
+    }
+    elementScore.innerHTML = Math.floor(Math.random() * (getallen.length) + 1);
+
 };
 
